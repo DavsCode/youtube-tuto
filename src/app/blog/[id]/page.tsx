450855927b67d2ed3ce7c26fa920507dad240ee5
DavsCode <codewithdavs@gmail.com>
2023-07-18 16:18:11 +0200

    initial commit next ts app

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DetailPage = () => {
+  return (
+    <div>DetailPage</div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h2>Hello from Next TS App.</h2>
+  )
+}
