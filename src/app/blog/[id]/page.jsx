d475a6e51c0fb33d6b427b9507e1496a055e173e
DavsCode <codewithdavs@gmail.com>
2023-07-18 14:18:55 +0200

    initial commit next app

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogDetail = () => {
+  return (
+    <div>BlogDetail</div>
+  )
+}
+
+export default BlogDetail
---
+++ src/app/page.jsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h2>Home Page</h2>
+  )
+}
